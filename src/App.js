b5b1223bbc1acf96a1effc9ee8fe0fa43d6e55b3
williamChang80 <william_chang80@rocketmail.com>
2020-06-03 15:18:52 +0700

    Inital Commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Card from "./Components/Card";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header"></header>
+      <div>
+        <Card />
+      </div>
+    </div>
+  );
+}
+
+export default App;
